9da2b77e00b419767d3dde3199cbc08c4fd9dfe7
Zhia Chong <zhiachong@gmail.com>
2014-11-06 17:21:20 -0800

    Ready

--- watcher.rb
+++ watcher.rb
@@ -3,12 +3,13 @@
 require 'rubygems'
 require 'rb-fsevent'
 
-def sync(local, host, remote, exclude, ssh)
+def sync(local, host, remote, exclude, ssh, fileName)
 
 # Construct the bash command that runs rsync.
 cmd = "rsync -rltDvzO --chmod=ugo=rwX --exclude-from " + exclude + " -e " + "\"" + ssh + "\"" + " " + "\"" + local + "\"" + " " + host + ":" + remote
 # Run the command.
 system cmd
+# system '/usr/bin/osascript -e "display notification \"File: ' + fileName.split(//).last(30).join("").to_s + ' is synced\" with title \"Process Completed\" "'
 
 end
 
@@ -21,7 +22,13 @@ ssh	= 'ssh -i /Users/zhiachong/.ssh/paperg_rsa'
 
 fsevent = FSEvent.new
 fsevent.watch local do |directories|
-  sync(local, host, remote, exclude, ssh)
+    directories.each do |directoryName|
+        directoryInString = directoryName.to_s
+        puts directoryInString
+        if !((directoryInString.include? ".git") || (directoryInString.include? ".idea")) then
+            sync(local, host, remote, exclude, ssh, directoryInString)
+        end
+    end
 end
 
 fsevent.run
